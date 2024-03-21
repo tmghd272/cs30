0d4cf7709eb55bdf6b79cf0097fd50711fbb6341
Ninja <44890370+tmghd272@users.noreply.github.com>
2024-03-21 12:38:56 -0600

    Update sketch.js

--- Game/sketch.js
+++ Game/sketch.js
@@ -17,7 +17,7 @@ function setup() {
   scoreElem.position(20, 20);
   scoreElem.id = 'score';
   scoreElem.style('color', 'rgb(255,0,0)');
-  scoreElem.size(20,0);
+  scoreElem.size(30, auto);
 
   createCanvas(windowWidth,windowHeight - 22.5); //fixed window since button exists
   frameRate(15);// snake speed
