9bfe61755014daa4d35683ddcbee1ebb86e4f925
Ninja <44890370+tmghd272@users.noreply.github.com>
2024-03-21 10:17:00 -0600

    Update sketch.js

--- Game/sketch.js
+++ Game/sketch.js
@@ -1,92 +1,136 @@
-//collide2d demo
+let numSegments = 10;
+let direction = 'right';
 
-let hit = false;
+const xStart = 0; //starting x coordinate for snake
+const yStart = 250; //starting y coordinate for snake
+const diff = 10;
 
-function setup() {
-  createCanvas(400, 400);
-}
+let xCor = [];
+let yCor = [];
 
-
-function draw() {
-  background(255);
-  rect(200, 200, 100, 150);
-  circle(mouseX, mouseY, 100);
-
-  hit = collideRectCircle(200, 200, 100, 150, mouseX, mouseY, 100);
-
-  if (hit) {
-    stroke("gray");
-  }
-  else {
-    stroke("black");
-  }
-
-  console.log("colliding?", hit);
-}
-
-let x;
-let y;
-let dx;
-let dy;
-let radius = 30;
-let r = 120;
-let g = 255;
-let b = 120;
+let xFruit = 0;
+let yFruit = 0;
+let scoreElem;
 
 function setup() {
-  createCanvas(1280, 720);
-  x = width / 2;
-  y = height / 2;
-  dx = random(-10, 10;
-  dy = random(-10, 10);
-  changeCircleColour();
-  noStroke();
+  scoreElem = createDiv('Score = 0');
+  scoreElem.position(20, 20);
+  scoreElem.id = 'score';
+  scoreElem.style('color', 'white');
+
+  createCanvas(500, 500);
+  frameRate(15);
+  stroke(255);
+  strokeWeight(10);
+  updateFruitCoordinates();
+
+  for (let i = 0; i < numSegments; i++) {
+    xCor.push(xStart + i * diff);
+    yCor.push(yStart);
+  }
 }
 
 function draw() {
-  background(660);
-
-  drawCircle();
-  moveCircle();
-  bounceOffWall();
-}
-
-function keyTyped() {
-  if (key === "d") {
-    dx = random(-10, 10);
-    dy = random(-10, 10);
+  background(0);
+  for (let i = 0; i < numSegments - 1; i++) {
+    line(xCor[i], yCor[i], xCor[i + 1], yCor[i + 1]);
   }
-  if (key === "c") {
-    changeCircleColour();
+  updateSnakeCoordinates();
+  checkGameStatus();
+  checkForFruit();
+}
+
+function updateSnakeCoordinates() {
+  for (let i = 0; i < numSegments - 1; i++) {
+    xCor[i] = xCor[i + 1];
+    yCor[i] = yCor[i + 1];
   }
-}
-
-function drawCircle() {
-  //display circle
-  fill(r, g, b);
-  circle(x, y, radius * 2);
-}
-
-function moveCircle() {
-  //move circle
-  x += dx;
-  y += dy;
-}
-
-function bounceOffWall() {
-  //bounce if needed
-  if (x + radius >= width || x - radius <= 0) {
-    dx = -1 * dx;
-    changeCircleColour();
-  }
-  if (y + radius >= height || y - radius <= 0) {
-    dy = -1 * dy;
-    changeCircleColour();
+  switch (direction) {
+    case 'right':
+      xCor[numSegments - 1] = xCor[numSegments - 2] + diff;
+      yCor[numSegments - 1] = yCor[numSegments - 2];
+      break;
+    case 'up':
+      xCor[numSegments - 1] = xCor[numSegments - 2];
+      yCor[numSegments - 1] = yCor[numSegments - 2] - diff;
+      break;
+    case 'left':
+      xCor[numSegments - 1] = xCor[numSegments - 2] - diff;
+      yCor[numSegments - 1] = yCor[numSegments - 2];
+      break;
+    case 'down':
+      xCor[numSegments - 1] = xCor[numSegments - 2];
+      yCor[numSegments - 1] = yCor[numSegments - 2] + diff;
+      break;
   }
 }
 
-function changeCircleColour() {
-  r = random(255);
-  g = random(255);
-  b = random(255);
+function checkGameStatus() {
+  if (
+    xCor[xCor.length - 1] > width ||
+    xCor[xCor.length - 1] < 0 ||
+    yCor[yCor.length - 1] > height ||
+    yCor[yCor.length - 1] < 0 ||
+    checkSnakeCollision()
+  ) {
+    noLoop();
+    const scoreVal = parseInt(scoreElem.html().substring(8));
+    scoreElem.html('Nice try! Your score was : ' + scoreVal);
+  }
+}
+
+/*
+ If the snake hits itself, that means the snake head's (x,y) coordinate
+ has to be the same as one of its own segment's (x,y) coordinate.
+*/
+function checkSnakeCollision() {
+  const snakeHeadX = xCor[xCor.length - 1];
+  const snakeHeadY = yCor[yCor.length - 1];
+  for (let i = 0; i < xCor.length - 1; i++) {
+    if (xCor[i] === snakeHeadX && yCor[i] === snakeHeadY) {
+      return true;
+    }
+  }
+}
+
+function checkForFruit() {
+  point(xFruit, yFruit);
+  if (xCor[xCor.length - 1] === xFruit && yCor[yCor.length - 1] === yFruit) {
+    const prevScore = parseInt(scoreElem.html().substring(8));
+    scoreElem.html('Score = ' + (prevScore + 1));
+    xCor.unshift(xCor[0]);
+    yCor.unshift(yCor[0]);
+    numSegments++;
+    updateFruitCoordinates();
+  }
+}
+
+function updateFruitCoordinates() {
+  xFruit = floor(random(10, (width - 100) / 10)) * 10;
+  yFruit = floor(random(10, (height - 100) / 10)) * 10;
+}
+
+function keyPressed() {
+  switch (keyCode) {
+    case 37:
+      if (direction !== 'right') {
+        direction = 'left';
+      }
+      break;
+    case 39:
+      if (direction !== 'left') {
+        direction = 'right';
+      }
+      break;
+    case 38:
+      if (direction !== 'down') {
+        direction = 'up';
+      }
+      break;
+    case 40:
+      if (direction !== 'up') {
+        direction = 'down';
+      }
+      break;
+  }
 }
