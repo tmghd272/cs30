29739c7197d3428fda931e82295c5e14fea970f4
Ninja <44890370+tmghd272@users.noreply.github.com>
2024-03-22 14:11:00 -0600

    t
    t

--- Game2/sketch.js
+++ Game2/sketch.js
@@ -5,11 +5,19 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let x;
+let y;
+let r;
+let g;
+let b;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background(255,240,220);
+  
+  square(random)
+
 }
