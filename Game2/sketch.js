d7250ccaffab94f62ecef68ef8e514b852c2a273
Ninja <44890370+tmghd272@users.noreply.github.com>
2024-04-08 13:49:34 -0600

    Update sketch.js

--- Game2/sketch.js
+++ Game2/sketch.js
@@ -5,19 +5,19 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let x;
-let y;
-let a = random(200, 240);
-let b = random(200, 240);
-let c = random(200, 255);
+let x = random(0, 100);
+let y = random(0, 100);
+let r = random(200, 240);
+let g = random(200, 240);
+let b = random(200, 255);
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(a,b,c); 
-  square(random, random)
+  background(r,g,b); 
+  square(x, y, 100)
 
   
 }
