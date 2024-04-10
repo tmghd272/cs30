246f1e5a2da2c30c821eac738ebf10baa505fcf7
Ninja <44890370+tmghd272@users.noreply.github.com>
2024-04-10 14:03:46 -0600

    r
    r

--- Grid/sketch.js
+++ Grid/sketch.js
@@ -2,19 +2,27 @@
 // Enzo Batuigas
 // Apr 9, 2024
 
-
+// if you are hard-coding a level, I'd use something like this
 
 // let grid = [[1, 0, 0, 1],
-// ____________[0, 1, 0, 1],
-// ____________[0, 0, 0, 1],
-// ____________[1, 1, 0, 0],
-// ____________[1, 0, 1, 1],
-// ____________[0, 0, 0, 1]];
+//             [0, 1, 0, 1],
+//             [1, 1, 0, 0],
+//             [1, 0, 1, 1],
+//             [0, 0, 0, 1],
+//             [0, 0, 1, 1],
+//             [0, 1, 0, 1],
+//             [0, 0, 0, 1]];
 
+//if randomizing the grid, do this:
+let grid;
 let cellSize;
+const GRID_SIZE = 10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+  
+  //this is dumb -- should check if this is the right size!
   cellSize = height/grid.length;
 }
 
@@ -23,18 +31,77 @@ function draw() {
   displayGrid();
 }
 
-function displayGrid() {
-  for (let y = 0; y < 7; y++) {
-    for (let x = 0; x < 4; x++) {
-      if (grid[y][x] === 1) {
-        fill("black")
-      }
-      else (grid[y][x] === 1) {
-        fill("white")
-      }
+function keyPressed() {
+  if (key === "r") {
+    grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+  }
 
-    }
+  if (key === "e") {
+    grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+  }
+}
+
+function mousePressed() {
+  let x = Math.floor(mouseX/cellSize);
+  let y = Math.floor(mouseY/cellSize);
+
+  // console.log(x, y);
+
+  // don't fall of the edge of the grid
+  if (x < GRID_SIZE && y < GRID_SIZE){
+    toggleCell(x, y);
+  }
+
+}
+
+function toggleCell(x, y) {
+  // toggle the color of the cell
+  if (grid[y][x] === 0) {
+    grid[y][x] = 1;
+  }
+  else {
+    grid[y][x] = 0;
+  }
+}
+
+function displayGrid() {
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      if (grid[y][x] === 1) {
+        fill("black");
+      }
+      else {
+        fill("white");
+      }
       square(x * cellSize, y * cellSize, cellSize);
     }
   }
+}
+
+function generateRandomGrid(cols, rows) {
+  let emptyArray = [];
+  for (let y = 0; y < rows; y++) {
+    emptyArray.push([]);
+    for (let x = 0; x < cols; x++) {
+      //half the time, be a 1. Other half, be a 0.
+      if (random(100) < 50) {
+        emptyArray[y].push(0);
+      }
+      else {
+        emptyArray[y].push(1);
+      }
+    }
+  }
+  return emptyArray;
+}
+
+function generateEmptyGrid(cols, rows) {
+  let emptyArray = [];
+  for (let y = 0; y < rows; y++) {
+    emptyArray.push([]);
+    for (let x = 0; x < cols; x++) {
+      emptyArray[y].push(0);
+    }
+  }
+  return emptyArray;
 }
