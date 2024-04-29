2a0ba42505fe302ae79f835f10117d29b2c3e3b5
Ninja <44890370+tmghd272@users.noreply.github.com>
2024-04-29 00:28:11 -0600

    Update sketch.js

--- Game2/sketch.js
+++ Game2/sketch.js
@@ -5,12 +5,46 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let character;
+let obstacles = [];
+let score = 0;
+let gameOver = false;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
+
+  character = new Character();
+
+  setInterval(spawnObstacle, 1000);
 }
 
 function draw() {
-  noLoop();
-  background(random(100,250), random(100,250), random(100,250));
-  
+  background("gray");
+
+  if (!gameOver) {
+    character.display();
+    character.move();
+
+    moveObstaclesWithNoise();
+    displayObstacles();
+    
+    for (let i = obstacles.length - 1; i >= 0; i--) {
+      if (character.hits(obstacles[i])) {
+        gameOver = true;
+      }
+    }
+
+    textSize(20);
+    text("Score: " + score, 10, 30);
+  } else {
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text("Game Over", width / 2, height / 2);
+    text("Your score was: " + score, width / 2, height / 2 + 40);
+  }
+}
+
+function mousePressed() {
+  character.moveTo(mouseX, mouseY);
 }
