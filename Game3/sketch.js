54fcdc82861ba5449330962affe778e035196b5d
Ninja <44890370+tmghd272@users.noreply.github.com>
2024-04-30 12:00:47 -0600

    t

--- Game3/sketch.js
+++ Game3/sketch.js
@@ -6,25 +6,16 @@ let grid;
 let cellSize;
 const GRID_SIZE = 15;
 const PLAYER = 10;
-const OPEN_TILE = 0;
-const IMPASSIBLE = 1;
+const WALL = 1;
 let player = {
   x: 0,
   y: 0,
 };
-let grassImg;
-let pavingImg;
 let character;
-let backgroundMusic;
-let cantWalkSound;
 let state = "start screen";
 
 function preload() {
-  grassImg = loadImage("Assets/Images/grass.png");
-  pavingImg = loadImage("Assets/Images/cobblestone.png");
   character = loadImage("Assets/Images/player.png");
-  backgroundMusic = loadSound("Assets/Sounds/theme.wav");
-  cantWalkSound = loadSound("Assets/Sounds/barrier.wav");
 }
 
 function setup() {
@@ -34,13 +25,9 @@ function setup() {
     createCanvas(windowHeight, windowHeight);
   }
 
-  grid = generateRandomMaze(GRID_SIZE, GRID_SIZE);
+  generateMaze(GRID_SIZE, GRID_SIZE);
+
   cellSize = height / grid.length;
-
-  grid[player.y][player.x] = PLAYER;
-
-  backgroundMusic.setVolume(0.4);
-  cantWalkSound.setVolume(1.0);
 }
 
 function draw() {
@@ -56,58 +43,27 @@ function draw() {
   }
 }
 
-function displayGrid() {
-  for (let y = 0; y < grid.length; y++) {
-    for (let x = 0; x < grid[y].length; x++) {
-      if (grid[y][x] === IMPASSIBLE) {
-        image(grassImg, x * cellSize, y * cellSize, cellSize, cellSize);
-      } else if (grid[y][x] === OPEN_TILE) {
-        image(pavingImg, x * cellSize, y * cellSize, cellSize, cellSize);
-      } else if (grid[y][x] === PLAYER) {
-        image(character, x * cellSize, y * cellSize, cellSize, cellSize);
-      }
-    }
-  }
-}
-
-function generateRandomMaze(cols, rows) {
-  let maze = [];
-  for (let y = 0; y < rows; y++) {
-    maze.push([]);
-    for (let x = 0; x < cols; x++) {
-      if (x % 2 !== 0 && y % 2 !== 0) {
-        maze[y][x] = OPEN_TILE;
-      } else {
-        maze[y][x] = IMPASSIBLE;
-      }
-    }
-  }
-  return maze;
-}
-
 function keyPressed() {
-  if (keyIsDown === "r") {
-    grid = generateRandomMaze(GRID_SIZE, GRID_SIZE);
-  }
+  if (state === "game") {
+    let newX = player.x;
+    let newY = player.y;
 
-  if (keyIsDown === "e") {
-    grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
-  }
+    if (keyIsDown(UP_ARROW)) {
+      newY -= 1;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+      newY += 1;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+      newX += 1;
+    }
+    if (keyIsDown(LEFT_ARROW)) {
+      newX -= 1;
+    }
 
-  if (keyIsDown(87)) {   //up
-    movePlayer(player.x + 0, player.y - 1); //0 on x axis, -1 on y axis
-  }
-
-  if (keyIsDown(83)) {   //down
-    movePlayer(player.x + 0, player.y + 1); //0 on x axis, 1 on y axis
-  }
-
-  if (keyIsDown(68)) {   //right
-    movePlayer(player.x + 1, player.y + 0); //1 on x axis, 0 on y axis
-  }
-
-  if (keyIsDown(65)) {   //left
-    movePlayer(player.x - 1, player.y + 0); //-1 on x axis, 0 on y axis
+    if (isValidMove(newX, newY)) {
+      movePlayer(newX, newY);
+    }
   }
 
   if (key === " " && state === "start screen") {
@@ -117,47 +73,54 @@ function keyPressed() {
 }
 
 function movePlayer(x, y) {
-  if (x < GRID_SIZE && y < GRID_SIZE &&
-      x >= 0 && y >= 0 && grid[y][x] === OPEN_TILE) {
-      let oldX = player.x;
-      let oldY = player.y;
+  player.x = x;
+  player.y = y;
 
-      player.x = x;
-      player.y = y;
-
-      grid[oldY][oldX] = OPEN_TILE;
-      grid[player.y][player.x] = PLAYER;
-  } else {
-    cantWalkSound.play();
+  if (grid[y][x] === WALL) {
+    // If the player hits a wall, handle accordingly (e.g., play sound)
   }
 }
 
-function mousePressed() {
-  let x = Math.floor(mouseX/cellSize);
-  let y = Math.floor(mouseY/cellSize);
-
-  toggleCell(x, y);
+function isValidMove(x, y) {
+  return (
+    x >= 0 &&
+    y >= 0 &&
+    x < GRID_SIZE &&
+    y < GRID_SIZE &&
+    grid[y][x] !== WALL
+  );
 }
 
-function toggleCell(x, y) {
-  if (x < GRID_SIZE && y < GRID_SIZE &&
-      x >= 0 && y >= 0) {
-    if (grid[y][x] === OPEN_TILE) {
-      grid[y][x] = IMPASSIBLE;
-    }
-    else if (grid[y][x] === IMPASSIBLE) {
-      grid[y][x] = OPEN_TILE;
+function displayGrid() {
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      if (grid[y][x] === WALL) {
+        // Draw wall
+      } else if (grid[y][x] === PLAYER) {
+        // Draw player
+        image(character, x * cellSize, y * cellSize, cellSize, cellSize);
+      }
     }
   }
 }
 
-function generateEmptyGrid(cols, rows) {
-  let emptyArray = [];
+function generateMaze(cols, rows) {
+  grid = [];
   for (let y = 0; y < rows; y++) {
-    emptyArray.push([]);
+    let row = [];
     for (let x = 0; x < cols; x++) {
-      emptyArray[y].push(0);
+      // Create outer walls
+      if (x === 0 || y === 0 || x === cols - 1 || y === rows - 1) {
+        row.push(WALL);
+      } else {
+        // Create maze structure
+        row.push(PLAYER);
+      }
     }
+    grid.push(row);
   }
-  return emptyArray;
+
+  // Set player position
+  player.x = 1;
+  player.y = 1;
 }
