c371ecfc8830feba14480cdb3a32c7372916f25d
Ninja <44890370+tmghd272@users.noreply.github.com>
2024-05-29 13:58:40 -0600

    t
    t

---
+++ Sierpinski Triangle/sketch.js
@@ -0,0 +1,59 @@
+// Sierpinski Triangle
+
+let initialTriangle = [
+  {x: 800, y: 20},
+  {x: 100, y: 650},
+  {x: 1500, y: 650}
+];
+
+let theDepth = 0;
+let theColors = ["black", "red", "blue", "yellow", "brown", "purple", "orange", "green"];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  sierpinski(initialTriangle, theDepth);
+}
+
+function mousePressed() {
+  if (theDepth < 7) {
+    theDepth++;
+  }
+}
+
+function sierpinski(points, depth) {
+  fill(theColors[depth]);
+  triangle(points[0].x, points[0].y, 
+           points[1].x, points[1].y,
+           points[2].x, points[2].y,
+  );
+
+  if (depth > 0) {
+    //draw the lower left triangle
+    sierpinski([midpoint(points[0], points[1]),
+                midpoint(points[1], points[2]),
+                points[1]],
+                depth - 1);
+
+    //draw the upper triangle
+    sierpinski([midpoint(points[0], points[1]),
+                midpoint(points[0], points[2]),
+                points[0]],
+                depth - 1);
+
+    //draw the lower right triangle
+    sierpinski([midpoint(points[0], points[2]),
+                midpoint(points[1], points[2]),
+                points[2]],
+                depth - 1);
+  }
+}
+
+function midpoint(point1, point2) {
+  let newX = (point1.x + point2.x) / 2;
+  let newY = (point1.y + point2.y) / 2;
+  return {x: newX, y: newY};
+}
